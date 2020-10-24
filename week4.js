b123feaba3c4e4b485b12eb786ef6c7fd847be02
JuanMoscatelli <juan.moscatelli@gmail.com>
2020-10-24 19:46:32 -0300

    Exc.1-b

--- week4.js
+++ week4.js
@@ -7,6 +7,13 @@ suma = primerNumero + segundoNumero;
 console.log(suma);
 
 
-/*Crear dos variables de tipo String y concatenarlas guardando el resultado en una 3er variable.
-Crear dos variables de tipo String y sumar el largo de cada variable (cantidad de letras del string) guardando el resultado la suma una 3er variable (utilizar length).
+/*Crear dos variables de tipo String y concatenarlas guardando el resultado en una 3er variable.*/
+var primerString,segundoString,concatenacion;
+primerString = 'neo';
+segundoString = 'natal';
+concatenacion = primerString + segundoString;
+console.log(concatenacion);
+
+
+/*Crear dos variables de tipo String y sumar el largo de cada variable (cantidad de letras del string) guardando el resultado la suma una 3er variable (utilizar length).
 */
