7e69e60dc06736a4f370aa17824406e42f2c1cb1
JuanMoscatelli <juan.moscatelli@gmail.com>
2020-10-24 19:40:37 -0300

    Exc.1-a

---
+++ week4.js
@@ -0,0 +1,12 @@
+/*Variables y Operadores
+Crear dos variables numéricas y utilizar el operador suma para guardar el valor de la suma de ambos números en una 3er variable.*/
+var primerNumero, segundoNumero, suma;
+primerNumero = 3;
+segundoNumero = 2;
+suma = primerNumero + segundoNumero;
+console.log(suma);
+
+
+/*Crear dos variables de tipo String y concatenarlas guardando el resultado en una 3er variable.
+Crear dos variables de tipo String y sumar el largo de cada variable (cantidad de letras del string) guardando el resultado la suma una 3er variable (utilizar length).
+*/
