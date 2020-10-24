53000517d2ff0482629b3127f55a7862a5604d9c
JuanMoscatelli <juan.moscatelli@gmail.com>
2020-10-24 20:24:06 -0300

    Exc.2-b

--- week4.js
+++ week4.js
@@ -5,7 +5,13 @@ var tenWords;
 tenWords = 'animalistic';
 console.log(tenWords.toUpperCase());
 
-/*Crear una variable de tipo string con al menos 10 caracteres y generar un nuevo string con los primeros 5 caracteres guardando el resultado en una nueva variable (utilizar substring).
+/*Crear una variable de tipo string con al menos 10 caracteres y generar un nuevo string con los primeros 5 caracteres guardando el resultado en una nueva variable (utilizar substring).*/
+var newword;
+newword = tenWords.substring(0,5);
+console.log(newword);
+
+
+/*
 Crear una variable de tipo string con al menos 10 caracteres y generar un nuevo string con los últimos 3 caracteres guardando el resultado en una nueva variable (utilizar substring).
 Crear una variable de tipo string con al menos 10 caracteres y generar un nuevo string con la primera letra en mayúscula y las demás en minúscula. Guardar el resultado en una nueva variable (utilizar substring, toUpperCase, toLowerCase y el operador +).
 Crear una variable de tipo string con al menos 10 caracteres y algún espacio en blanco. Encontrar la posición del primer espacio en blanco y guardarla en una variable (utilizar indexOf).
