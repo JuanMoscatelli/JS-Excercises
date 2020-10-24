dbec42d6a035d88e211a5e64fb737b8c542af813
JuanMoscatelli <juan.moscatelli@gmail.com>
2020-10-24 20:20:39 -0300

    Exc.2-a

--- week4.js
+++ week4.js
@@ -1,24 +1,13 @@
-/*Variables y Operadores
-Crear dos variables numéricas y utilizar el operador suma para guardar el valor de la suma de ambos números en una 3er variable.*/
-var primerNumero, segundoNumero, suma;
-primerNumero = 3;
-segundoNumero = 2;
-suma = primerNumero + segundoNumero;
-console.log(suma);
 
+/*Strings
+Crear una variable de tipo string con al menos 10 caracteres y convertir todo el texto en mayúscula (utilizar toUpperCase).*/
+var tenWords;
+tenWords = 'animalistic';
+console.log(tenWords.toUpperCase());
 
-/*Crear dos variables de tipo String y concatenarlas guardando el resultado en una 3er variable.*/
-var primerString,segundoString,concatenacion;
-primerString = 'neo';
-segundoString = 'natal';
-concatenacion = primerString + segundoString;
-console.log(concatenacion);
-
-/*Crear dos variables de tipo String y sumar el largo de cada variable (cantidad de letras del string) guardando el resultado la suma una 3er variable (utilizar length).
+/*Crear una variable de tipo string con al menos 10 caracteres y generar un nuevo string con los primeros 5 caracteres guardando el resultado en una nueva variable (utilizar substring).
+Crear una variable de tipo string con al menos 10 caracteres y generar un nuevo string con los últimos 3 caracteres guardando el resultado en una nueva variable (utilizar substring).
+Crear una variable de tipo string con al menos 10 caracteres y generar un nuevo string con la primera letra en mayúscula y las demás en minúscula. Guardar el resultado en una nueva variable (utilizar substring, toUpperCase, toLowerCase y el operador +).
+Crear una variable de tipo string con al menos 10 caracteres y algún espacio en blanco. Encontrar la posición del primer espacio en blanco y guardarla en una variable (utilizar indexOf).
+Crear una variable de tipo string con al menos 2 palabras largas (10 caracteres y algún espacio entre medio). Utilizar los métodos de los ejercicios anteriores para generar un nuevo string que tenga la primera letra de ambas palabras en mayúscula y las demás letras en minúscula (utilizar indexOf, substring, toUpperCase, toLowerCase y el operador +).
 */
-
-var sumaCadena
-
-sumaCadena = primerString.length + segundoString.length;
-
-console.log(sumaCadena);
