49ede1e987ec7fefe1b0ebaee7a3bf042a289e19
JuanMoscatelli <juan.moscatelli@gmail.com>
2020-10-26 03:41:18 -0300

    Exc.4-1,2

--- week4.js
+++ week4.js
@@ -1,39 +1,38 @@
-/*Arrays
-Dado el siguiente array: ["Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre"] mostrar por consola los meses 5 y 11 (utilizar console.log)
+//If Else
+/*Crear un número aleatorio entre 0 y 1 utilizando la función Math.random(), si el valor es mayor o igual que 0,5 mostrar una alerta con el mensaje “Greater than 0,5” y sino un alerta con el mensaje “Lower than 0,5”*/
+var number
+number = Math.random(0,1)
+if (number<= 0,5) {
+    alert("Grater than 0,5")
+}else{
+    alert("lower than 0,5")   
+}
+console.log(number)
+
+
+/*
+Crear una variable “Age” que contenga un número entero entre 0 y 100 y muestre los siguientes mensajes de alerta:
+“Bebe” si la edad es menor a 2 años
+“Nino” si la edad es entre 2 y 12 años
+“Adolecente” si la edad es entre 13 y 19 años
+“Joven” si la edad está entre 20 y 30 años
+“Adulto” entre 31 y 60 años
+“Adulto mayor” entre 61 y 75 años
+“Anciano” si es mayor a 75 años
 */
-var myArray = ["Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre"];
-console.log(myArray[5]);
-console.log(myArray[11]);
-
-
-
-/*Ordenar el array de meses alfabéticamente y mostrarlo por consola (utilizar sort).*/
-//myArray.sort();
-//console.log(myArray.sort());
-
-/*
-Agregar un elemento al principio y al final del array (utilizar unshift y push).*/
-myArray.unshift("preEnero");
-myArray.push("postDiciembre");
-console.log(myArray);
-
-
-/*
-Quitar un elemento del principio y del final del array (utilizar shift y pop).*/
-myArray.shift();
-myArray.pop();
-console.log(myArray);
-
-
-/*
-Invertir el orden del array (utilizar reverse)*/
-//myArray.reverse();
-//console.log(myArray);
-/*
-Unir todos los elementos del array en un único string donde cada mes este separado por un guión - (utilizar join).*/
-console.log(myArray.join('-'));
-
-/*
-Crear una copia del array de meses que contenga desde Mayo hasta Noviembre (utilizar slice)
-*/
-console.log(myArray.slice(4,11));
+var age = 31;
+if (age<2){
+    alert('Bebe')
+}else if (age < 12){
+    alert('Nino')
+}else if (age < 19){
+    alert('Adolescente')
+}else if (age < 30){
+    alert('Joven')
+}else if (age < 60){
+    alert('Adulto')
+}else if (age < 75){
+    alert('Adulto')
+}else {
+    alert('Anciano')
+}
