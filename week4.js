4acc8280c084a9751581c2a17bb9620018fd9751
JuanMoscatelli <juan.moscatelli@gmail.com>
2020-10-26 13:16:52 -0300

    Exc.5-3

--- week4.js
+++ week4.js
@@ -1,6 +1,6 @@
 /*Al array anterior convertir la primera letra de cada palabra en mayúscula y mostrar una alerta por cada palabra modificada.*/
-
-
+var myColors = ['rosa','verde','gris','negro','azul'];
+/*
 var myColors = ['rosa','verde','gris','negro','azul'], indexMyColors, color, firstLetter;
 for (indexMyColors = 0; indexMyColors < 5; indexMyColors++){
     color = myColors[indexMyColors];
@@ -8,8 +8,15 @@ for (indexMyColors = 0; indexMyColors < 5; indexMyColors++){
     color = color.replace(firstLetter,firstLetter.toUpperCase());
     alert(color);
 }
-
+*/
 /*
 Crear una variable llamada “sentence” que tenga un string vacío, luego al array del punto a) recorrerlo con un bucle for para ir guardando cada palabra dentro de la variable sentence. Al final mostrar una única alerta con la cadena completa.
-Crear una array vacío y con un bucle for de 10 repeticiones llenar el array con el número de la repetición, es decir que al final de la ejecución del bucle for deberia haber 10 elementos dentro del array, desde el número 0 hasta al numero 9. Mostrar por la consola del navegador el al array final (utilizar console.log).
+*/
+var sentence = [' ']
+for (indexMyColors = 0; indexMyColors <5; indexMyColors++){
+    sentence.push(myColors[indexMyColors]);
+}
+alert(sentence);
+
+/*Crear una array vacío y con un bucle for de 10 repeticiones llenar el array con el número de la repetición, es decir que al final de la ejecución del bucle for deberia haber 10 elementos dentro del array, desde el número 0 hasta al numero 9. Mostrar por la consola del navegador el al array final (utilizar console.log).
 */
