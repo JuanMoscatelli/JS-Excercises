07f1cbd23a06bab9bfe784da5c7a8d4d0326cf8f
JuanMoscatelli <juan.moscatelli@gmail.com>
2020-10-26 00:13:45 -0300

    Exc.2-4

--- week4.js
+++ week4.js
@@ -19,7 +19,18 @@ last3Words= tenWords.substring(8,11);
 console.log(last3Words);
 
 
-/*Crear una variable de tipo string con al menos 10 caracteres y generar un nuevo string con la primera letra en mayúscula y las demás en minúscula. Guardar el resultado en una nueva variable (utilizar substring, toUpperCase, toLowerCase y el operador +).
+/*
+Crear una variable de tipo string con al menos 10 caracteres y generar un nuevo string con la primera letra en mayúscula y las demás en minúscula. Guardar el resultado en una nueva variable (utilizar substring, toUpperCase, toLowerCase y el operador +).
+*/
+var wordOne,a,b;
+a = tenWords.toUpperCase();
+b = tenWords.toLowerCase();
+wordOne = a.substring(0,1) + b.substring(1,11);
+console.log(wordOne);
+ 
+
+
+/*
 Crear una variable de tipo string con al menos 10 caracteres y algún espacio en blanco. Encontrar la posición del primer espacio en blanco y guardarla en una variable (utilizar indexOf).
 Crear una variable de tipo string con al menos 2 palabras largas (10 caracteres y algún espacio entre medio). Utilizar los métodos de los ejercicios anteriores para generar un nuevo string que tenga la primera letra de ambas palabras en mayúscula y las demás letras en minúscula (utilizar indexOf, substring, toUpperCase, toLowerCase y el operador +).
 */
