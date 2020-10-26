89b8689bf05399d6432d5ce8309908ca9d52f054
JuanMoscatelli <juan.moscatelli@gmail.com>
2020-10-26 03:16:25 -0300

    Exc.3-5,6,7

--- week4.js
+++ week4.js
@@ -8,8 +8,8 @@ console.log(myArray[11]);
 
 
 /*Ordenar el array de meses alfabéticamente y mostrarlo por consola (utilizar sort).*/
-myArray.sort();
-console.log(myArray.sort());
+//myArray.sort();
+//console.log(myArray.sort());
 
 /*
 Agregar un elemento al principio y al final del array (utilizar unshift y push).*/
@@ -26,7 +26,14 @@ console.log(myArray);
 
 
 /*
-Invertir el orden del array (utilizar reverse)
-Unir todos los elementos del array en un único string donde cada mes este separado por un guión - (utilizar join).
+Invertir el orden del array (utilizar reverse)*/
+//myArray.reverse();
+//console.log(myArray);
+/*
+Unir todos los elementos del array en un único string donde cada mes este separado por un guión - (utilizar join).*/
+console.log(myArray.join('-'));
+
+/*
 Crear una copia del array de meses que contenga desde Mayo hasta Noviembre (utilizar slice)
 */
+console.log(myArray.slice(4,11));
