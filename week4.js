39519392d5b7d9b71be350d8ad6dee06a29b7d85
JuanMoscatelli <juan.moscatelli@gmail.com>
2020-10-26 03:08:23 -0300

    Exc.3-2,3,4

--- week4.js
+++ week4.js
@@ -1,15 +1,31 @@
 /*Arrays
 Dado el siguiente array: ["Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre"] mostrar por consola los meses 5 y 11 (utilizar console.log)
 */
-var myarray = ["Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre"];
-console.log(myarray[5]);
-console.log(myarray[11]);
+var myArray = ["Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre"];
+console.log(myArray[5]);
+console.log(myArray[11]);
 
 
 
-/*Ordenar el array de meses alfabéticamente y mostrarlo por consola (utilizar sort).
-Agregar un elemento al principio y al final del array (utilizar unshift y push).
-Quitar un elemento del principio y del final del array (utilizar shift y pop).
+/*Ordenar el array de meses alfabéticamente y mostrarlo por consola (utilizar sort).*/
+myArray.sort();
+console.log(myArray.sort());
+
+/*
+Agregar un elemento al principio y al final del array (utilizar unshift y push).*/
+myArray.unshift("preEnero");
+myArray.push("postDiciembre");
+console.log(myArray);
+
+
+/*
+Quitar un elemento del principio y del final del array (utilizar shift y pop).*/
+myArray.shift();
+myArray.pop();
+console.log(myArray);
+
+
+/*
 Invertir el orden del array (utilizar reverse)
 Unir todos los elementos del array en un único string donde cada mes este separado por un guión - (utilizar join).
 Crear una copia del array de meses que contenga desde Mayo hasta Noviembre (utilizar slice)
