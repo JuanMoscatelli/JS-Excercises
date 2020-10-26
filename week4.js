0783044dc1d7bdbeb5fe17dd6e5a12d9dec2774e
JuanMoscatelli <juan.moscatelli@gmail.com>
2020-10-26 14:29:13 -0300

    Exc.6-2

--- week4.js
+++ week4.js
@@ -2,9 +2,13 @@
 Crear una función suma que reciba dos valores numéricos y retorne el resultado. Ejecutar la función y guardar el resultado en una variable, mostrando el valor de dicha variable en la consola del navegador.
 */
 function suma(num1,num2) {
+    if (isNaN(num1)|| isNaN(num2)){
+        alert('Parametro tiene error')
+        return NaN
+    }
     return  num1 + num2;
 }
-console.log(suma(4,5))
+console.log(suma(4,'n'))
 
 /*A la función suma anterior, agregarle una validación para controlar si alguno de los parámetros no es un número, mostrar una alerta aclarando que uno de los parámetros tiene error y retornar el valor NaN como resultado.
 */
