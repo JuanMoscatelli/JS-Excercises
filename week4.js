664c1bf07e00262fe089c6e3a59067c52dd04420
JuanMoscatelli <juan.moscatelli@gmail.com>
2020-10-26 12:47:08 -0300

    Exc.5-1

--- week4.js
+++ week4.js
@@ -1,38 +1,16 @@
-//If Else
-/*Crear un número aleatorio entre 0 y 1 utilizando la función Math.random(), si el valor es mayor o igual que 0,5 mostrar una alerta con el mensaje “Greater than 0,5” y sino un alerta con el mensaje “Lower than 0,5”*/
-var number
-number = Math.random(0,1)
-if (number<= 0,5) {
-    alert("Grater than 0,5")
-}else{
-    alert("lower than 0,5")   
+/*
+For
+Crear un array que contenga 5 palabras y recorrer dicho array utilizando un bucle for de JavaScript para mostrar una alerta utilizando cada una de las palabras.
+*/
+
+var myColors = ['Rosa','Verde','Gris','Negro','Azul'];
+for (var indexMyColors = 0; indexMyColors < 5; indexMyColors++){
+    alert(myColors[indexMyColors]);
 }
-console.log(number)
+/*Al array anterior convertir la primera letra de cada palabra en mayúscula y mostrar una alerta por cada palabra modificada.*/
 
 
 /*
-Crear una variable “Age” que contenga un número entero entre 0 y 100 y muestre los siguientes mensajes de alerta:
-“Bebe” si la edad es menor a 2 años
-“Nino” si la edad es entre 2 y 12 años
-“Adolecente” si la edad es entre 13 y 19 años
-“Joven” si la edad está entre 20 y 30 años
-“Adulto” entre 31 y 60 años
-“Adulto mayor” entre 61 y 75 años
-“Anciano” si es mayor a 75 años
+Crear una variable llamada “sentence” que tenga un string vacío, luego al array del punto a) recorrerlo con un bucle for para ir guardando cada palabra dentro de la variable sentence. Al final mostrar una única alerta con la cadena completa.
+Crear una array vacío y con un bucle for de 10 repeticiones llenar el array con el número de la repetición, es decir que al final de la ejecución del bucle for deberia haber 10 elementos dentro del array, desde el número 0 hasta al numero 9. Mostrar por la consola del navegador el al array final (utilizar console.log).
 */
-var age = 31;
-if (age<2){
-    alert('Bebe')
-}else if (age < 12){
-    alert('Nino')
-}else if (age < 19){
-    alert('Adolescente')
-}else if (age < 30){
-    alert('Joven')
-}else if (age < 60){
-    alert('Adulto')
-}else if (age < 75){
-    alert('Adulto')
-}else {
-    alert('Anciano')
-}
