417a49fd991e406fa43e950fda4e142a49556ace
JuanMoscatelli <juan.moscatelli@gmail.com>
2020-10-26 14:26:11 -0300

    Exc.6-1

--- week4.js
+++ week4.js
@@ -1,27 +1,19 @@
-/*Al array anterior convertir la primera letra de cada palabra en mayúscula y mostrar una alerta por cada palabra modificada.*/
-//var myColors = ['rosa','verde','gris','negro','azul'];
-/*
-var myColors = ['rosa','verde','gris','negro','azul'], indexMyColors, color, firstLetter;
-for (indexMyColors = 0; indexMyColors < 5; indexMyColors++){
-    color = myColors[indexMyColors];
-    firstLetter = color.slice(0,1);
-    color = color.replace(firstLetter,firstLetter.toUpperCase());
-    alert(color);
+/*Funciones
+Crear una función suma que reciba dos valores numéricos y retorne el resultado. Ejecutar la función y guardar el resultado en una variable, mostrando el valor de dicha variable en la consola del navegador.
+*/
+function suma(num1,num2) {
+    return  num1 + num2;
 }
+console.log(suma(4,5))
+
+/*A la función suma anterior, agregarle una validación para controlar si alguno de los parámetros no es un número, mostrar una alerta aclarando que uno de los parámetros tiene error y retornar el valor NaN como resultado.
 */
-/*
-Crear una variable llamada “sentence” que tenga un string vacío, luego al array del punto a) recorrerlo con un bucle for para ir guardando cada palabra dentro de la variable sentence. Al final mostrar una única alerta con la cadena completa.
+
+
+
+
+
+/*Crear una función validate integer que reciba un número como parámetro y devuelva verdadero si es un número entero.
+A la función suma del ejercicio 6b) agregarle una llamada que valide que los números sean enteros. En caso que haya decimales mostrar un alerta con el error y retorna el número convertido a entero (redondeado).
+Convertir la validación del ejercicio 6b) en una función separada y llamarla dentro de la función suma probando que todo siga funcionando igual.
 */
-/*var sentence = []
-for (indexMyColors = 0; indexMyColors <5; indexMyColors++){
-    sentence.push(myColors[indexMyColors]);
-}
-alert(sentence);
-*/
-/*Crear una array vacío y con un bucle for de 10 repeticiones llenar el array con el número de la repetición, es decir que al final de la ejecución del bucle for deberia haber 10 elementos dentro del array, desde el número 0 hasta al numero 9. Mostrar por la consola del navegador el al array final (utilizar console.log).
-*/
-var empty = []
-for (index=0;index<10; index++){
-    empty.push(index)
-}
-console.log(empty)
